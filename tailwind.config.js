a5573c5c4ab813b14dac8a00db547033706ac76c
morad <adidimorad7@gmail.com>
2024-11-13 01:36:17 +0100

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 import forms from '@tailwindcss/forms';
 
 export default {
-  
-  content: ["./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",],
-    theme: {
-      extend: {
-        colors: {
-          darkGray: '#2b2e4a',
-          accentPurple: '#7f5af0',
-          bgPrimary: '#1a1c2c',
-          bgSecondary: '#282a36',
-          textAccent: '#8be9fd',
-        },
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        darkGray: '#1F1F1F',
+        bgPrimary: '#121212',
+        bgSecondary: '#242424',
+        accentPurple: '#7D40F5',
+        textAccent: '#9CA3AF',
+      },
+      borderRadius: {
+        xl: '1rem',
+      },
+      boxShadow: {
+        lg: '0 10px 15px -3px rgba(0, 0, 0, 0.5)',
       },
     },
-    plugins: [forms],
-}
-
+  },
+  plugins: [forms],
+};
